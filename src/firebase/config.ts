546090e036faa74c98a15e25678f3feb8f64f5f3
Jinx <nouhaelrh300103@gmail.com>
2025-11-06 23:19:53 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -23,11 +23,11 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 =======
 export const firebaseConfig = {
-  "projectId": "studio-5737549229-1a40f",
-  "appId": "1:209392512231:web:042e8bc3bf77916122c642",
-  "apiKey": "AIzaSyC218rurkyDxWeR2q8FIYyAAyg8FUKtn6o",
-  "authDomain": "studio-5737549229-1a40f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "209392512231"
+  apiKey: "AIzaSyAM6yuxcRPJW5drSotFovd_7jhmij48Vkg",
+  authDomain: "nouhaila-66422.firebaseapp.com",
+  projectId: "nouhaila-66422",
+  storageBucket: "nouhaila-66422.appspot.com",
+  messagingSenderId: "1096165685651",
+  appId: "1:1096165685651:web:ef7a395a3f13193a109066"
 };
 >>>>>>> 3059669 (Set up a Firebase backend)
