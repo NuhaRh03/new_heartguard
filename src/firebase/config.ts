e992eb025b61f852f8e19bc616f5c4443a63afb8
Jinx <nouhaelrh300103@gmail.com>
2025-11-09 22:23:30 +0000

    can u remove any conflit text

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBCudWulukTCOPBdQ5kGZjIQQGHbMuSlFg",
-  authDomain: "fccm-91cb4.firebaseapp.com",
-  databaseURL: "https://fccm-91cb4-default-rtdb.firebaseio.com",
-  projectId: "fccm-91cb4",
-  storageBucket: "fccm-91cb4.firebasestorage.app",
-  messagingSenderId: "469479046693",
-  appId: "1:469479046693:web:dc432cae17df7d548889cc",
-  measurementId: "G-0M9WPWW81R"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-=======
 export const firebaseConfig = {
   apiKey: "AIzaSyAM6yuxcRPJW5drSotFovd_7jhmij48Vkg",
   authDomain: "nouhaila-66422.firebaseapp.com",
@@ -30,4 +7,3 @@ export const firebaseConfig = {
   messagingSenderId: "1096165685651",
   appId: "1:1096165685651:web:ef7a395a3f13193a109066"
 };
->>>>>>> 3059669 (Set up a Firebase backend)
