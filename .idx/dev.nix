f1ba06690bc1ecd9e5c2a2bfafc23783659e5e88
Jinx <nouhaelrh300103@gmail.com>
2025-11-15 00:49:08 +0000

    logo for firestore

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,4 +1,4 @@
-# Tjjjjjjjjo learn more about how to use Nix to configure your environment
+# Tjjjjjjjo learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
 {pkgs}: {
   # Which nixpkgs channel to use.
